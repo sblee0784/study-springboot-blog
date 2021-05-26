07fa71b9afe5d5e89eb41ddff1ff376f0d3b7139
sblee <sblee@synergyai.co>
2021-05-26 15:58:02 +0900

    lecture 21 - reply table create

--- src/main/java/com/cos/blog/model/User.java
+++ src/main/java/com/cos/blog/model/User.java
@@ -11,7 +11,16 @@ import javax.persistence.Id;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 
-//	ORM -> Java(다른언어) Object -> 테이블로 매핑해주는 기술
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+//ORM -> Java(다른언어) Object -> 테이블로 매핑해주는 기술
 @Entity	// User 클래스가 MySQL에 테이블이 생성이 된다.
 public class User {
 	
