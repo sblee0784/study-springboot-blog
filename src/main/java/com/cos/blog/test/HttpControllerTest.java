35d18760097d47c9009e5844e2a5ace6aba96c13
sblee <sblee@synergyai.co>
2021-05-26 09:49:47 +0900

    lecture 14 - http request 2

--- src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // 사용자가 요청 -> 응답(HTML 파일)
@@ -15,20 +17,20 @@ public class HttpControllerTest {
 
 	// http://localhost:8080/http/get (select)
 	@GetMapping("/http/get")
-	public String getTest() {
-		return "get 요청";
+	public String getTest(Member m) {	// id=1&username=ssar&password=1234&email=ssar@nate.com
+		return "get 요청 : "+m.getId()+", "+m.getUsername()+", "+m.getPassword()+", "+m.getEmail();
 	}
 
 	// http://localhost:8080/http/post (insert)
 	@PostMapping("/http/post")
-	public String postTest() {
-		return "post 요청";
+	public String postTest(@RequestBody Member m) {	// MessageConverter (스프링부트)
+		return "post 요청 : "+m.getId()+", "+m.getUsername()+", "+m.getPassword()+", "+m.getEmail();
 	}
 	
 	// http://localhost:8080/http/put (update)
 	@PutMapping("/http/put")
-	public String putTest() {
-		return "put 요청";
+	public String putTest(@RequestBody Member m) {
+		return "put 요청 : "+m.getId()+", "+m.getUsername()+", "+m.getPassword()+", "+m.getEmail();
 	}
 	
 	// http://localhost:8080/http/delete (delete)
