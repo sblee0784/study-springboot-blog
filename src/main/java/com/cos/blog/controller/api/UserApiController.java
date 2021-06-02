4c16151468a3e67d37c18fce71479764a52a2deb
sblee <sblee@synergyai.co>
2021-06-02 10:34:44 +0900

    lecture 48 - spring security

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -19,9 +19,6 @@ public class UserApiController {
 	@Autowired
 	private UserService userService;
 	
-	@Autowired
-	private HttpSession session;
-	
 	@PostMapping("/api/user")
 	public ResponseDto<Integer> save(@RequestBody User user) {	// username, password, email
 		System.out.println("UserApiController : save 호출됨");
@@ -31,15 +28,15 @@ public class UserApiController {
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), result);	// 자바오브젝트를 JSON으로 변환해서 리턴 (Jackson)
 	}
 	
-	// 다음시간에 스프링 시큐리티 이용해서 로그인!!
-	@PostMapping("/api/user/login")
-	public ResponseDto<Integer> login(@RequestBody User user) {
-		System.out.println("UserApiController : login 호출됨");
-		User principal = userService.로그인(user);	// principal (접근주체)
-		
-		if (principal != null) {
-			session.setAttribute("principal", principal);
-		}
-		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
-	}
+//	// 다음시간에 스프링 시큐리티 이용해서 로그인!!
+//	@PostMapping("/api/user/login")
+//	public ResponseDto<Integer> login(@RequestBody User user, HttpSession session) {
+//		System.out.println("UserApiController : login 호출됨");
+//		User principal = userService.로그인(user);	// principal (접근주체)
+//		
+//		if (principal != null) {
+//			session.setAttribute("principal", principal);
+//		}
+//		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
+//	}
 }
