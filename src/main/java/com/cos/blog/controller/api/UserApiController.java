4bb861f47486bb46a189225290bac865dfef02d6
sblee <sblee@synergyai.co>
2021-06-02 16:49:34 +0900

    lecture 49 - login page customizing

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -19,7 +19,7 @@ public class UserApiController {
 	@Autowired
 	private UserService userService;
 	
-	@PostMapping("/api/user")
+	@PostMapping("/auth/joinProc")
 	public ResponseDto<Integer> save(@RequestBody User user) {	// username, password, email
 		System.out.println("UserApiController : save 호출됨");
 		// 실제로 DB에 insert를 하고 아래에서 return이 되면 되요.
@@ -27,16 +27,4 @@ public class UserApiController {
 		int result = userService.회원가입(user);
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), result);	// 자바오브젝트를 JSON으로 변환해서 리턴 (Jackson)
 	}
-	
-//	// 다음시간에 스프링 시큐리티 이용해서 로그인!!
-//	@PostMapping("/api/user/login")
-//	public ResponseDto<Integer> login(@RequestBody User user, HttpSession session) {
-//		System.out.println("UserApiController : login 호출됨");
-//		User principal = userService.로그인(user);	// principal (접근주체)
-//		
-//		if (principal != null) {
-//			session.setAttribute("principal", principal);
-//		}
-//		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
-//	}
 }
