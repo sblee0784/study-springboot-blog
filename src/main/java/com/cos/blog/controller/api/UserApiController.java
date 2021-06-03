2534eb499173b65ff172902255e409a2c5a09c4b
sblee <sblee@synergyai.co>
2021-06-03 13:58:14 +0900

    lecture 50 - password hashing

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,8 +23,6 @@ public class UserApiController {
 	@PostMapping("/auth/joinProc")
 	public ResponseDto<Integer> save(@RequestBody User user) {	// username, password, email
 		System.out.println("UserApiController : save 호출됨");
-		// 실제로 DB에 insert를 하고 아래에서 return이 되면 되요.
-		user.setRole(RoleType.USER);
 		int result = userService.회원가입(user);
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), result);	// 자바오브젝트를 JSON으로 변환해서 리턴 (Jackson)
 	}
--- src/main/java/com/cos/blog/service/UserService.java
+++ src/main/java/com/cos/blog/service/UserService.java
@@ -1,9 +1,11 @@
 package com.cos.blog.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
 
@@ -13,10 +15,17 @@ public class UserService {
 
 	@Autowired
 	private UserRepository userRepository;
+
+	@Autowired
+	private BCryptPasswordEncoder encoder;
 	
 	@Transactional
 	public int 회원가입(User user) {
 		try {
+			String rawPassword = user.getPassword();	//	1234 원문
+			String encPassword = encoder.encode(rawPassword);	//	해쉬
+			user.setPassword(encPassword);
+			user.setRole(RoleType.USER);
 			userRepository.save(user);
 			return 1;
 		} catch (Exception e) {
