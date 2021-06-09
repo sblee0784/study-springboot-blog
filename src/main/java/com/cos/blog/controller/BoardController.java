fb8586f3bc7c8e2a92b4352886ae82bbc409babb
sblee <sblee@synergyai.co>
2021-06-09 15:08:14 +0900

    lecture 54 - board list

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -1,16 +1,23 @@
 package com.cos.blog.controller;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.cos.blog.service.BoardService;
 
 @Controller
 public class BoardController {
+	
+	@Autowired
+	private BoardService boardService;
 
+//	컨트롤러에서 세션을 어떻게 찾는지?
 //	@AuthenticationPrincipal PrincipalDetail principal
 	@GetMapping({"", "/"})
-	public String index() {	//	컨트롤러에서 세션을 어떻게 찾는지?
-		// /WEB-INF/views/index.jsp
+	public String index(Model model) {
+		model.addAttribute("boards", boardService.글목록());
 		return "index";
 	}
 	
