b8a5b419bb4e07685a1962459356ff17cdfcfdf3
sblee <sblee@synergyai.co>
2021-06-09 14:54:25 +0900

    lecture 53 - board saveForm

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -4,15 +4,19 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.cos.blog.config.auth.PrincipalDetail;
-
 @Controller
 public class BoardController {
 
-	@GetMapping("/")
-	public String index(@AuthenticationPrincipal PrincipalDetail principal) {	//	컨트롤러에서 세션을 어떻게 찾는지?
+//	@AuthenticationPrincipal PrincipalDetail principal
+	@GetMapping({"", "/"})
+	public String index() {	//	컨트롤러에서 세션을 어떻게 찾는지?
 		// /WEB-INF/views/index.jsp
-		System.out.println("로그인 사용자 아이디 : "+principal.getUsername());
 		return "index";
+	}
+	
+	// USER 권한이 필요
+	@GetMapping("/board/saveForm")
+	public String saveForm() {
+		return "board/saveForm";
 	}
 }
