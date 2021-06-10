d7b53e61ae2f824bd76cd158e50f11a4a5c6aa26
sblee <sblee@synergyai.co>
2021-06-10 15:12:54 +0900

    lecture 61 - user edit 2

--- src/main/java/com/cos/blog/controller/UserController.java
+++ src/main/java/com/cos/blog/controller/UserController.java
@@ -1,7 +1,10 @@
 package com.cos.blog.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.cos.blog.config.auth.PrincipalDetail;
 
 //	인증이 안된 사용자들이 출입할 수 있는 경로를 /auth/** 허용
 // 그냥 주소가 / 이면 index.jsp 허용
@@ -21,7 +24,7 @@ public class UserController {
 	}
 	
 	@GetMapping("/user/updateForm")
-	public String updateForm() {
+	public String updateForm(@AuthenticationPrincipal PrincipalDetail principal) {
 		return "user/updateForm";
 	}
 }
